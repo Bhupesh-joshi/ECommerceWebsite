f1a5d3be1d4612d932e988ed0e71841c75a4857e
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2022-09-27 13:47:26 +0530

    my first commit e-commerceWebsite

---
+++ js.js
@@ -0,0 +1,77 @@
+document.getElementsByClassName('myAccountDropDown')[0].style.display='block';
+document.getElementsByClassName('myAccountDropDown').style.display='none';
+document.getElementsByIdName('myAccountDropDown').style.display='block';
+document.getElementsByIdName('myAccountDropDown').style.display='none';
+
+let dropDown = document.createElement('div');
+console.log(dropDown);
+
+var button = document.createElement('button');  
+button.setAttribute('type', 'button');  buttons.push(button);
+buttons.push(button);   
+length = document.createElement('col');
+
+console.log('length: ' + length);
+
+var button = document.createElement('button');
+button.setAttribute('type', 'button'); buttons.push(button);
+var width = document.createElement('div');
+let height = document.createElement('div');
+
+const button = document.createError
+console.error(button.messages || []);
+console.error(button.getAttribute('type') || 'button');  
+buttons.push(button);
+
+console.log(buttons.lengths === 1 ? buttons[0] : null);
+
+let button = document.createError
+let error = document.createError();
+let message = document.createError();
+lengths.forEach(function(length) {message.appendChild(document.createTextNode(length)); });
+
+console.log(buttons + message); 
+onafterprint = document.createElement('div');
+
+console.log(buttons.push(button));
+
+ArrayBuffer.prototype.push.apply(buttons, buttons.slice(0, buttons.length - buttons.length));
+AbortController = document.createElement('div');    
+AbstractRangeController = document.createElement('div');
+AbstractRangeController.appendChild(AbortController);
+MediaKeyMessageEvent = document.createElement('div');
+const MediaKeyMessage = document.createElement('div');  
+
+console.log(MediaKeyMessageEvent);
+
+FunctionPrototype  = document.createElement('div');
+FilePrototype = document.createElement('div');
+HTMLAllCollectionPrototype = document.createElement('div');
+NaN = document.createElement('div');
+IDBCursor = document.createElement('col');
+Lock = document.createElement('div');
+
+console.log(FunctionPrototype); 
+console.log(FilePrototype);
+NetworkInformation = document.createElement('div');
+
+document.body.appendChild(document.createTextNode(FunctionPrototype.toString()));
+
+HTMLTableRowElement = document.createElement('table');
+console = document.createElement('table');
+RTCDTMFToneChangeEvent = document.createElement('div');
+document.body.appendChild(RTCDTMFToneChangeEvent); 
+
+document.getElementById('myFunction');
+document.createAttributeNS(MediaRecorder);
+DocumentFragment = document.createElement('div');
+
+onpointerout.bind(document, 'pointerout');
+document.addEventListener('pointerout', onpointerout,true);
+
+document.addEventListener('pointermove','onPointerMove',false);
+documentElement.addEventListener('pointermove',onPointerMove,false);
+
+document.getElementById('pointerup').addEventListener('pointerup',onPointerMove,true);
+
+
